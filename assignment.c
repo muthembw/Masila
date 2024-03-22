d3a9a162a163680775799087d4933bd93152bed0
muthembw <164518935+muthembw@users.noreply.github.com>
2024-03-22 17:33:47 +0300

    Add files via upload
    BRIDGET MASILA SCT221-0228/2022

---
+++ assignment.c
@@ -0,0 +1,89 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL)
+        return newNode(data);
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+    return root;
+}
+
+struct Node* deleteNode(struct Node* root, int key) {
+    if (root == NULL)
+        return root;
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else {
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct Node* temp = root->right;
+        while (temp->left != NULL)
+            temp = temp->left;
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return -1;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight) ? leftHeight + 1 : rightHeight + 1;
+}
+
+void printLevelAndHeight(struct Node* root, int key, int level) {
+    if (root == NULL)
+        return;
+    if (root->data == key)
+        printf("Level: %d, Height: %d\n", level, height(root));
+    else if (key < root->data)
+        printLevelAndHeight(root->left, key, level + 1);
+    else
+        printLevelAndHeight(root->right, key, level + 1);
+}
+
+int main() {
+    int arr[] = {30, 20, 40, 10, 25, 35, 45, 5, 15};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    struct Node* root = NULL;
+
+
+    for (int i = 0; i < n; i++)
+        root = insert(root, arr[i]);
+
+    root = deleteNode(root, 20);
+
+    printf("Height of BST: %d\n", height(root));
+
+    printLevelAndHeight(root, 25, 0);
+
+    return 0;
+}
